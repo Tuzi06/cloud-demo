a3ccbb148623515f64ad30ce725a474ef0969474
tuzi06 <damienwang21@gmail.com>
2023-08-04 02:05:10 -0700

    asdf

--- test.py
+++ test.py
@@ -82,4 +82,4 @@ def test():
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=False, host='127.0.0.1', port=port)
+    app.run(debug=False, host='0.0.0.0', port=port)
