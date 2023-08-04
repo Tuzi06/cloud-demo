42848c1cd4ff7e2f8c3d9f147f4395ba191f671c
tuzi06 <damienwang21@gmail.com>
2023-08-04 02:27:04 -0700

    asf

--- test.py
+++ test.py
@@ -14,3 +14,6 @@ def test():
         if response.status_code != 200:
             return 'scraper detected'
     return 'success'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000,debug=False)
