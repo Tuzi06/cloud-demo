705563548f1d24c65119dc6b01c7607295564e47
tuzi06 <damienwang21@gmail.com>
2023-08-10 19:49:37 -0700

    dsf

--- lowlevel/ins.py
+++ lowlevel/ins.py
@@ -26,14 +26,19 @@ def findComment(soup,content):
     container = soup.find('div',class_='x78zum5 xdt5ytf x1iyjqo2')
     comments = container.find_all('div',recursive=False)
     res = []
+    print(len(comments[1:]))
     for comment in comments[1:]:
-        if comment.find_all('div',class_='x9f619 xjbqb8w x78zum5 x168nmei x13lgxp2 x5pf9jr xo71vjh xwib8y2 x1y1aw1k x1uhb9sk x1plvlek xryxfnj x1c4vz4f x2lah0s x1q0g3np xqjyukv x6s0dn4 x1oa3qoh x1nhvcw1')!= []:
-            replys = comment.find_all('div',class_='x9f619 xjbqb8w x78zum5 x168nmei x13lgxp2 x5pf9jr xo71vjh xsag5q8 xz9dl7a x1uhb9sk x1plvlek xryxfnj x1c4vz4f x2lah0s x1q0g3np xqjyukv x1qjc9v5 x1oa3qoh x1nhvcw1')
-            res[-1]['reply'] = [{reply.find('div',class_='xt0psk2').get_text():reply.find('div',class_='x9f619 xjbqb8w x78zum5 x168nmei x13lgxp2 x5pf9jr xo71vjh x1uhb9sk x1plvlek xryxfnj x1c4vz4f x2lah0s xdt5ytf xqjyukv x1cy8zhl x1oa3qoh x1nhvcw1').get_text()}for reply in replys]
-        else:
-            res.append({'comment': {comment.find('div',class_='xt0psk2').get_text():comment.find('div',class_='x9f619 xjbqb8w x78zum5 x168nmei x13lgxp2 x5pf9jr xo71vjh x1uhb9sk x1plvlek xryxfnj x1c4vz4f x2lah0s xdt5ytf xqjyukv x1cy8zhl x1oa3qoh x1nhvcw1').get_text()},'reply':[]})
+        try:
+            if comment.find_all('div',class_='x1i10hfl xjbqb8w x6umtig x1b1mbwd xaqea5y xav7gou x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz x87ps6o x1d5wrs8')!= []:
+                replys = comment.find_all('div',class_='x9f619 xjbqb8w x78zum5 x168nmei x13lgxp2 x5pf9jr xo71vjh xsag5q8 xz9dl7a x1uhb9sk x1plvlek xryxfnj x1c4vz4f x2lah0s x1q0g3np xqjyukv x1qjc9v5 x1oa3qoh x1nhvcw1')
+                res[-1]['reply'] = [{reply.find('div',class_='xt0psk2').get_text():reply.find('div',class_='x9f619 xjbqb8w x78zum5 x168nmei x13lgxp2 x5pf9jr xo71vjh x1uhb9sk x1plvlek xryxfnj x1c4vz4f x2lah0s xdt5ytf xqjyukv x1cy8zhl x1oa3qoh x1nhvcw1').get_text()}for reply in replys]
+            else:
+                res.append({'comment':{comment.find('div',class_='xt0psk2').get_text() : comment.find('div',class_='x9f619 xjbqb8w x78zum5 x168nmei x13lgxp2 x5pf9jr xo71vjh x1uhb9sk x1plvlek xryxfnj x1c4vz4f x2lah0s xdt5ytf xqjyukv x1cy8zhl x1oa3qoh x1nhvcw1').get_text()},'reply':[]})
+            
+        except:
+            continue
 
-    content['comments'] = comments
+    content['comments'] = res
 
 def findPicture(driver,idx = 0,user = {'id':''}):
     #抓取所有帖子里的图片的链接
--- test.py
+++ test.py
@@ -6,13 +6,16 @@ from lowlevel.ins import findPicture
 import requests,pickle
 
 
-# cookies = pickle.load(open('lowlevel/ins_cookies.pkl','rb'))
-# driver = prepare_driver(cookies,1,False)[0]
-# driver.get('https://www.instagram.com/p/Cvt1PgPtUMj/')
-# time.sleep(5)
+cookies = pickle.load(open('lowlevel/ins_cookies.pkl','rb'))
+driver = prepare_driver(cookies,1,False)[0]
+driver.get('https://www.instagram.com/p/Cvt1PgPtUMj/')
+time.sleep(5)
 # pictures = findPicture(driver,0)
 # driver.quit()
 from bs4 import BeautifulSoup as bs
 html = open('res.html','r').read()
-response = requests.post('http://192.168.1.67:5000/dataJob',data = {'html':html,'user':{'id':'asdf'},'pics':[]},files = {'file':html})
-# driver.quit()
+requests.get('http://192.168.1.67:5000/start')
+requests.post('http://192.168.1.67:5000/dataJob',json = {'html':driver.page_source,'user':{'id':'asdf'},'pics':[]})
+response = requests.get('http://192.168.1.67:5000/download')
+print(response.text)
+driver.quit()
