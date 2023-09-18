b1927da3427e68ee4452c3b3ab326bf6b0ea5ae4
tuzi06 <damienwang21@gmail.com>
2023-09-17 21:51:25 -0700

    new exp

--- xhs/exp.py
+++ xhs/exp.py
@@ -1,85 +1,45 @@
-import json
 import pickle
 import time
-from selenium import webdriver
+from selenium.webdriver import Chrome,ChromeOptions
 from bs4 import BeautifulSoup as bs
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from multiprocessing import Process,Manager,Queue
 
-def process(browser,userlinks,userPass,userFail,log):
-    while not userlinks.empty():
-        user = userlinks.get()
-        browser.get(user)
-        links = []
-        fail = False
-        while len(links)<100:
-            time.sleep(2)
-            oldlength = len(links)
-            sections = browser.find_elements(By.CLASS_NAME,'note-item')
-            links+=[section.find_elements(By.TAG_NAME,'a')[1] for section in sections]
-            links = list(set(links))
+service = Service(executable_path='lowlevel/chromedriver-mac-arm64/chromedriver')
+cookies = pickle.load(open('lowlevel/cookies_linux.pkl','rb'))
+options = ChromeOptions()
+options.add_argument('disable-blink-features=AutomationControlled')
+options.add_arguments('headless')
 
-            if len(links) == oldlength:
-                userFail.append(user)
-                fail = True
-                break
+browser1 = Chrome(service=service,options=options)
+browser1.get('https://www.xiaohongshu.com/explore')
+for cookie in cookies: 
+    if isinstance(cookie.get('expiry'),float):
+        cookie['expiry'] = int(cookie['expiry'])
+    browser1.add_cookie(cookie)
 
-            browser.execute_script("arguments[0].scrollIntoView();",sections[-1])
-        if not fail:
-            userPass.append(user)
-        if str(len(links)) not in log:
-            log[str(len(links))] = 1
-        else:
-            log[str(len(links))] += 1
+browser2 = Chrome(service=service,options=options)
 
+total = 0
+sucess = 0
+# input('pause')
+# pickle.dump(browser1.get_cookies(),open('lowlevel/xhs_cookies.pkl','wb'))
+browser1.get('https://www.xiaohongshu.com/explore?channel_id=homefeed_recommend')
+time.sleep(5)
+while total<1000:
+    wrappers = browser1.find_elements(By.CLASS_NAME,'author-wrapper')
+    userlinks = [wrapper.find_element(By.TAG_NAME,'a').get_attribute('href') for wrapper in wrappers]
+    browser1.execute_script("arguments[0].scrollIntoView();",wrappers[-1])
+    for user in userlinks:
+        browser2.get(user)
+        time.sleep(2)
+        total +=1
+        soup = bs(browser2.page_source,'html.parser')
+        follow = soup.findAll('span',class_='count')[1].text
 
-def main():
-    users = json.load(open('users.json','r'))
-
-    log = Manager().dict()
-    options = webdriver.ChromeOptions()
-    options.add_argument('disable-blink-features=AutomationControlled')
-    options.add_argument('headless')
-    options.add_argument("--disable-dev-shm-usage")
-    browsers = [webdriver.Remote(command_executor=f"http://127.0.0.1:4444/wd/hub",options=options) for _ in range(10)]
-
-    # browsers = [webdriver.Chrome(options=options, service=Service(executable_path='lowlevel/chromedriver-linux64/chromedriver'))]
-
-    cookies = pickle.load(open('lowlevel/cookies_linux.pkl','rb'))
-    for browser in browsers:
-        browser.get('https://www.xiaohongshu.com/explore')
-        for cookie in cookies: 
-            if isinstance(cookie.get('expiry'),float):
-                cookie['expiry'] = int(cookie['expiry'])
-            browser.add_cookie(cookie)
-        browser.refresh()
-    userlinks = Queue()
-    for user in users:
-        userlinks.put(user)
-
-    userPass = Manager().list()
-    userFail = Manager().list()
-    processes = []
-    for browser in browsers:
-        processes.append(Process(target=process, args=[browser,userlinks,userPass,userFail,log]))
-    for p in processes:
-        p.start()
-
-    total = len(users)
-    length = 50
-    while not userlinks.empty():
-        percent = ("{0:." + str(2) + "f}").format(100 * ((total-userlinks.qsize()) / float(total)))
-        filledLength = int(length * (total-userlinks.qsize()) // total)
-        bar = '█' * filledLength + '-' * (length - filledLength)
-        print(f'\r{bar}| {percent}% finished', end = '\r')
-    for p in processes:
-        p.join()
-    print(f"\n\n{len(userPass)} users has more than 100 posts, {len(userFail)} users has less than 100 posts")
-    # open('log.json','w').write(json.dumps(log,indent=4))
-    print(log)
-
-    for browser in browsers:
-        browser.quit()
-if __name__ =='__main__':
-    main()
+        if 'W' in follow or '万' in follow:
+            sucess+=1
+        p1 =percent = ("{0:." + str(3) + "f}").format(100 * (sucess/total))
+        print(f"{p1}% of user has follow greater than 10000, and {total} users checked",end="\r")
+browser1.quit()
+browser2.quit()
